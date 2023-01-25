e2e7ed9acb7f6a6144900ed277a3e38d87e8568d
Harsha1309 <harshyadav1393@gmail.com>
2023-01-25 14:23:46 +0530

    Portfolio

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/harshyadav1309/" target="_blank" className="home__social-icon"><i class="uil uil-linkedin-alt"></i></a>
+        <a href="https://instagram.com/harsha_1309?utm_medium=copy_link" target="_blank" className="home__social-icon"><i class="uil uil-instagram"></i></a>
+        <a href="https://github.com/Harsha1309" target="_blank" className="home__social-icon"><i class="uil uil-github-alt"></i></a>
+    </div>
+  )
+}
+
+export default Social
